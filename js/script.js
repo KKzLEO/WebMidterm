9103ad37893fc5dab7b592fe4358866492188323
Leo <avril9950@gmail.com>
2018-04-26 07:50:50 +0800

    投票頁面 + RWD

--- js/script.js
+++ js/script.js
@@ -2,11 +2,12 @@ var scrollVal = 0;
 var isReset = false;
 
 $(".nav-bar li").hover(function(){
-    if(scrollVal > 0){
-        $(this).append('<div class="hover-wrap-sm"></div>');
-    }else{
-        $(this).append('<div class="hover-wrap"></div>');
-    }
+    $(this).append('<div class="hover-wrap-sm"></div>');
+    // if(scrollVal > 0){
+    //     $(this).append('<div class="hover-wrap-sm"></div>');
+    // }else{
+    //     $(this).append('<div class="hover-wrap"></div>');
+    // }
 },function(){
     $(".hover-wrap").remove();
     $(".hover-wrap-sm").remove();
@@ -51,6 +52,42 @@ $(".to-top10video").click(function(){
     window.location.href = './top10video.html';
 });
 
+$(".to-voting").click(function(){
+    window.location.href = './voting.html';
+});
+
 $(".btn-nav-toggle").click(function(){
     $(".nav-bar-sm").toggle();
 });
+
+$(".voting-submit").click(function(){
+    swal({
+        title: "確定要投票嗎?",
+        text: "一但投票，便不能更改了唷！",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+        buttons: ["取消", "確定"],
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+            totalVotes();
+            swal("已成功投票", {
+                icon: "success",
+            });
+        }
+      });
+});
+
+
+function totalVotes(){
+    $(".voting-card").each(function(){
+        
+        if($(this).find("input").prop("checked")){
+            var text = $(this).find(".votes-number").text();
+            // console.log(Number(text)+1);
+            $(this).find(".votes-number").text(Number(text)+1);
+            $(this).find("input").prop("checked",false)
+        }
+    });
+}
